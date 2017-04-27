47d97fdb44c09771ee759932407c5d66a40083d8
Graham Mace <graham@maceage.com>
2017-04-27 13:05:45 +1000

    Added more test cases

--- module1/test_phonebook.py
+++ module1/test_phonebook.py
@@ -17,3 +17,32 @@ class PhonebookTest(unittest.TestCase):
 
     def test_empty_phonebook_is_consistent(self):
         self.assertTrue(self.phonebook.is_consistent())
+
+    @unittest.skip
+    def test_is_consistent(self):
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Bob", "12345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Mary", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "12345")  # Identical to Bob
+        self.assertTrue(self.phonebook.is_consistent())
+        self.phonebook.add("Sue", "123")  # Prefix of Bob
+        self.assertFalse(self.phonebook.is_consistent())
+
+    def test_phonebook_with_normal_entries_is_consistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "012345")
+        self.assertTrue(self.phonebook.is_consistent())
+
+    @unittest.skip
+    def test_phonebook_with_duplicate_entries_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "012345")
+        self.assertFalse(self.phonebook.is_consistent())
+
+    @unittest.skip
+    def test_phonebook_with_numbers_that_prefix_one_another_is_inconsistent(self):
+        self.phonebook.add("Bob", "12345")
+        self.phonebook.add("Mary", "123")
+        self.assertFalse(self.phonebook.is_consistent())
